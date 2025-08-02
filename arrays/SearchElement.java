76a7b0ba960666b17dd9d8390a1220626862acc2
saivivek2420030018 <2420030018@klh.edu.in>
2025-08-02 10:31:26 +0530

    Add files via upload

---
+++ arrays/SearchElement.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SearchElement {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter number of elements: ");
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+
+			System.out.println("Enter " + n + " elements:");
+			for (int i = 0; i < n; i++) {
+			    arr[i] = sc.nextInt();
+			}
+
+			System.out.print("Enter element to search: ");
+			int key = sc.nextInt();
+			boolean found = false;
+
+			for (int num : arr) {
+			    if (num == key) {
+			        found = true;
+			        break;
+			    }
+			}
+
+			if (found)
+			    System.out.println("Element found.");
+			else
+			    System.out.println("Element not found.");
+		}
+    }
+}
---
+++ arrays/StudentMarks.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class StudentMarks {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+			int[] marks = new int[6];
+			int total = 0;
+
+			System.out.println("Enter marks for 6 subjects:");
+			for (int i = 0; i < 6; i++) {
+			    marks[i] = sc.nextInt();
+			    total += marks[i];
+			}
+
+			double average = total / 6.0;
+
+			System.out.println("Subject marks:");
+			for (int i = 0; i < 6; i++) {
+			    System.out.println("Subject " + (i + 1) + ": " + marks[i]);
+			}
+			System.out.println("Total: " + total);
+			System.out.println("Average: " + average);
+		}
+    }
+}
