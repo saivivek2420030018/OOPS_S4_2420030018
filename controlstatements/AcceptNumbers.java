c0213a7b73806ea55725c175b542c08d350e011e
saivivek2420030018 <2420030018@klh.edu.in>
2025-08-02 10:53:41 +0530

    Add files via upload

---
+++ controlstatements/AcceptNumbers.java
@@ -0,0 +1,20 @@
+package controlstatements;
+
+import java.util.Scanner;
+
+public class AcceptNumbers {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+			int num;
+
+			System.out.println("Enter numbers (0 to stop):");
+			do {
+			    num = sc.nextInt();
+			    if (num != 0)
+			        System.out.println("You entered: " + num);
+			} while (num != 0);
+		}
+
+    }
+}
